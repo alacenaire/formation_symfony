62675ea065a98aeb3921e50fade0870c6764cb8f
alacenaire <alacenaire@parkeon.com>
2017-11-17 17:13:00 +0100

    Formation Symfony

--- src/WSRestBundle/Controller/DefaultController.php
+++ src/WSRestBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use WSRestBundle\Entity\User;
 
 class DefaultController extends Controller
@@ -38,6 +39,45 @@ class DefaultController extends Controller
     	$users = $userRepository->findAll();
         $response = new Response();
         $response->setContent(json_encode($users));
+        $response->headers->set("Content-Type","application/json");
+        return $response;
+    }
+
+    /**
+     * @Route("/ws/users/{id}", name="showUser")
+     * @Method("get")
+     */
+    public function showUserAction($id)
+    {
+        //injection
+        $userServices = $this->get("user_services");
+        $user = $userServices->getUserById($id);
+        $response = new Response();
+        $response->setContent(json_encode($user));
+        $response->headers->set("Content-Type","application/json");
+        return $response;
+    }
+
+    /**
+     * @Route("/ws/createUser", name="createUser")
+     * @Method("post")
+     */
+    public function createUserAction()
+    {
+        $request = Request::createFromGlobals();
+        $jsonContent = $request->getcontent();
+
+        $userJson = json_decode($jsonContent);
+
+        $user = new User();
+        $user->setNom($userJson->nom);
+        $user->setPrenom($userJson->prenom);
+
+        //injection
+        $userServices = $this->get("user_services");
+        $userServices->createUser($user);
+        $response = new Response();
+        $response->setContent(json_encode($user));
         $response->headers->set("Content-Type","application/json");
         return $response;
     }
